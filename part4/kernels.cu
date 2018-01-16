90a15f15ccfb3a8cb2b37c4199517ddd1b498ea4
smedina <smedina@cs.upc.edu>
2018-01-16 11:32:40 +0100

    Still not working TT.TT

--- part4/kernels.cu
+++ part4/kernels.cu
@@ -2,8 +2,22 @@
 #include <float.h>
 #include <cuda.h>
 
+
+__global__ void gpu_HeatReduce (float *h, float *g, int N) {
+    // float diff, sum=0.0;
+    int j = threadIdx.x + blockDim.x * blockIdx.x + 1;
+    int i = threadIdx.y + blockDim.y * blockIdx.y + 1;
+    if (i < N - 1 && j < N - 1) {
+        g[i*N + j]= 0.25 * ( h[ i*N + (j-1) ]+  // left
+                             h[ i*N     + (j+1) ]+  // right
+                             h[ (i-1)*N + j     ]+  // top
+                             h[ (i+1)*N + j     ]); // bottom
+        // diff = g[i*N + j] - h[i*N + j];
+        // sum += diff * diff;
+    }
+}
+
 __global__ void gpu_Heat (float *h, float *g, int N) {
-	// float diff, sum=0.0;
 	int j = threadIdx.x + blockDim.x * blockIdx.x + 1;
 	int i = threadIdx.y + blockDim.y * blockIdx.y + 1;
 	if (i < N - 1 && j < N - 1) {
@@ -11,8 +25,6 @@ __global__ void gpu_Heat (float *h, float *g, int N) {
 				     h[ i*N     + (j+1) ]+  // right
 				     h[ (i-1)*N + j     ]+  // top
 				     h[ (i+1)*N + j     ]); // bottom
-		// diff = g[i*N + j] - h[i*N + j];
-		// sum += diff * diff;
 	}
 }
 
@@ -32,10 +44,11 @@ __global__ void gpu_Diff(float *h, float *g, int N) {
 	}
 }
 
-__global__ void gpu_Reduce(float *g) {
-  __shared__ int sdata[256];
+__global__ void gpu_Reduce(float *g, int N, int skip) {
+  __shared__ float sdata[256];
     int i = blockIdx.x*blockDim.x + threadIdx.x;
-    sdata[threadIdx.x] = g[i];
+    int i_skip = i*skip;
+    sdata[threadIdx.x] = i_skip < N ? g[i_skip]: 0.0;
 
     __syncthreads();
     // do reduction in shared mem
@@ -51,7 +64,28 @@ __global__ void gpu_Reduce(float *g) {
     }
 
     // write result for this block to global mem
-    if (threadIdx.x == 0)
-        atomicAdd(g,sdata[0]);
+    if (threadIdx.x == 0) g[i] = sdata[0];
 }
 
+__global__ void gpu_Reduce_Atomic(float *g, int N) {
+	__shared__ float sdata[256];
+	int i = blockIdx.x*blockDim.x + threadIdx.x;
+	sdata[threadIdx.x] = i < N ? g[i]: 0.0;
+
+	__syncthreads();
+	// do reduction in shared mem
+	for (int s=1; s < blockDim.x; s *=2)
+	{
+		int index = 2 * s * threadIdx.x;;
+
+		if (index < blockDim.x)
+		{
+			sdata[index] += sdata[index + s];
+		}
+		__syncthreads();
+	}
+
+	// write result for this block to global mem
+	if (threadIdx.x == 0)
+		atomicAdd(g,sdata[0]);
+}
